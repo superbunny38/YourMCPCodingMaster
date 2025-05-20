a6890d03d9d75ee31f1a6f76d5c8464fbe3e13d5
superbunny38 <superbunny38@gmail.com>
2025-05-19 21:12:17 -0400

    server init

--- server.py
+++ server.py
@@ -1,6 +1,24 @@
+import os
+import glob
 from fastmcp import FastMCP
 
-mcp = FastMCP("Demo 🚀")
+WORKSPACE_DIR = os.getcwd()
+CODE_STORAGE = os.path.join(WORKSPACE_DIR, "example_codes")
+print("Default workspace directory:", WORKSPACE_DIR)
+
+mcp = FastMCP("🛠️🤖 Your Coding Master 🤖🛠️")
+
+def get_code_path(code_name: str) -> str:
+    """
+    Get the path to the code file based on the code name.
+    
+    Args:
+        code_name: The name of the code file (without extension).
+    
+    Returns:
+        The full path to the code file.
+    """
+    
 
 @mcp.tool()
 def hello(name: str) -> str:
